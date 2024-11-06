68718a5cbf948673a307f4cc84401754f7e1b312
nagase <nagaken8937@gmail.com>
2024-11-06 23:33:02 +0900

    回転しながら運動方程式する動き

---
+++ Assets/ShopCircle.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+/// <summary>
+/// 運動方程式しながら回転する
+/// </summary>
+public class ShopCircle : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [Header("y軸の高さに比例する"), SerializeField]
+    private float animationLength;
+
+    private float timer = 0;
+
+    [SerializeField]
+    private Transform start;
+
+    [SerializeField]
+    private Transform end;
+
+    private Vector3 defaultPos;
+
+    private float vy;
+    private float vx;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    private void Start()
+    {
+        defaultPos = transform.position;
+        float t = animationLength;
+        float gravity = Physics.gravity.y;
+
+        // y軸
+        var startPos = start.position;
+        var endPos = end.position;
+        var diffY = (endPos - startPos).y;
+        vy = (diffY - gravity * 0.5f * t * t) / t;
+
+        // forward軸
+        // y軸を除いた長さ
+        Vector2 p1 = new Vector2(start.position.x, start.position.z);
+        Vector2 p2 = new Vector2(end.position.x, end.position.z);
+        float magnitude = p2.magnitude - p1.magnitude;
+        vx = magnitude / t;
+    }
+
+    private void Update()
+    {
+        float gravity = Physics.gravity.y;
+        timer += Time.deltaTime * speed;
+
+        if (timer >= animationLength)
+        {
+            timer = 0;
+            return;
+        }
+
+        var localEulerAngles = transform.localEulerAngles;
+        localEulerAngles.z += Time.deltaTime * rotateSpeed;
+        transform.localEulerAngles = localEulerAngles;
+
+        Vector3 forward = vx * timer * transform.forward;
+        Vector3 up = (vy * timer + 0.5f * gravity * timer * timer) * transform.up;
+
+        transform.position = forward;
+        transform.position += up;
+        transform.position += defaultPos;
+    }
+}
